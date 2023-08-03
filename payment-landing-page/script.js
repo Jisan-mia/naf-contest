6ea7d5cfa87a41bfd9cc945868fa7fb58e315ab4
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-08-04 04:02:58 +0600

    nonacademy contest question completed

--- payment-landing-page/script.js
+++ payment-landing-page/script.js
@@ -1,1 +1,28 @@
-// write your JavaScript here
+const mainMenu = document.querySelector(".mobile__navlist");
+// const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".hamburger");
+const menu_items = document.querySelectorAll(".mobile__navlist-item a");
+
+openMenu.addEventListener("click",toggle);
+// close menu when you click on a menu item
+menu_items.forEach((item) => {
+  item.addEventListener("click", toggle);
+});
+
+function toggle() {
+   mainMenu.classList.toggle("show");
+   openMenu.classList.toggle("hamburgerActive");
+
+}
+
+// prevent scroll when mobile menu showing
+function preventScroll(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
+}
+mainMenu.addEventListener("wheel", preventScroll);
+
+
+
